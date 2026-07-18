4df935cbcb7af17b2e0ae4a970f5eff54990724c
Vrdons <vrdons@proton.me>
2026-07-18 18:46:23 +0300

    i2c: Correct i2c disable for selene
    i2c6 used for sound drivers

--- drivers/i2c/busses/i2c-mtk.c
+++ drivers/i2c/busses/i2c-mtk.c
@@ -1366,9 +1366,15 @@ static int mt_i2c_parse_dt(struct device_node *np, struct mt_i2c *i2c)
 	of_property_read_u32(np, "rsel_cfg", &i2c->offset_rsel_cfg);
 	of_property_read_u32(np, "id", (u32 *)&i2c->id);
 
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
     if (i2c->id == 1 || i2c->id == 6 || i2c->id == 8) {
         return -1;
     }
+#else
+    if (i2c->id == 1 || i2c->id == 8) {
+        return -1;
+    }
+#endif
 
 	of_property_read_u16(np, "clk_sta_offset",
 		(u16 *)&i2c->clk_sta_offset);
