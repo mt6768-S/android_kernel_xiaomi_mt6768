mt6768-S/android_kernel_xiaomi_mt6768-k419

cam_cal: Import Xiaomi changes
* from selene-r-oss-update

Change-Id: I1d463eda04dcb8549fa801b7c2e8bb1af885be12

drivers/misc/mediatek/cam_cal/src/common/v1/eeprom_driver.c
@@ -621,6 +621,10 @@ static long EEPROM_drv_ioctl(struct file *file,
 		}
 
 		if (pcmdInf != NULL && g_lastDevID != ptempbuf->deviceID) {
+			#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+			//depth camera otp is in sensor,unable to get data due to register i2c fail,so disable it
+			if(ptempbuf->deviceID != 8) return -EFAULT;
+			#endif
 			if (EEPROM_set_i2c_bus(ptempbuf->deviceID,
 					       pcmdInf) != 0) {
 				pr_debug("deviceID Error!\n");

drivers/misc/mediatek/cam_cal/src/mt6768/cam_cal_list.c
@@ -27,6 +27,11 @@ extern unsigned int ov8856_qtech_front_read_otp_info(struct i2c_client *client, 
 #endif
 #endif
 
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+extern unsigned int gc02m1_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+extern unsigned int ov02b1b_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+#endif
+
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	/*Below is commom sensor */
 #ifdef CONFIG_TARGET_PRODUCT_LANCELOTCOMMON
@@ -39,6 +44,34 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{OV8856_QTECH_FRONT_SENSOR_ID, 0x6C, ov8856_qtech_front_read_otp_info},
 	{GC5035_OFILM_SENSOR_ID, 0xA4, Common_read_region},
 	{GC5035_QTECH_SENSOR_ID, 0xA4, Common_read_region},
+#endif
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	{OV50C40_OFILM_MAIN_SENSOR_ID, 0xA2, Common_read_region},
+	{S5KJN1_OFILM_MAIN_SENSOR_ID, 0xA2, Common_read_region},
+	{OV50C40_QTECH_MAIN_SENSOR_ID, 0xA2, Common_read_region},
+	{GC02M1_MACRO_AAC_SENSOR_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SY_SENSOR_ID, 0xA4, Common_read_region},
+	{IMX355_SUNNY_ULTRA_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX355_AAC_ULTRA_SENSOR_ID, 0xA0, Common_read_region},
+	{OV8856_OFILM_FRONT_SENSOR_ID, 0xA2, Common_read_region},
+	{OV8856_AAC_FRONT_SENSOR_ID, 0xA2, Common_read_region},
+	{GC02M1B_SENSOR_ID1, 0xA2, gc02m1_read_otp_info},
+	{OV02B1B_OFILM_SENSOR_ID, 0xA2, ov02b1b_read_otp_info},
+	{OV50C40_OFILM_MAIN_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{S5KJN1_OFILM_MAIN_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV50C40_QTECH_MAIN_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{GC02M1_MACRO_AAC_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SY_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{IMX355_SUNNY_ULTRA_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{IMX355_AAC_ULTRA_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{OV8856_OFILM_FRONT_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV8856_AAC_FRONT_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{GC02M1B_SUNNY_SENSOR_INDIA_ID, 0xA2, gc02m1_read_otp_info},
+	{OV02B1B_OFILM_SENSOR_INDIA_ID, 0xA2, ov02b1b_read_otp_info},
+	{S5KJN1_OFILM_MAIN_SENSOR_CN_ID, 0xA2, Common_read_region},
+	{OV50C40_QTECH_MAIN_SENSOR_CN_ID, 0xA2, Common_read_region},
+	{IMX355_SUNNY_ULTRA_SENSOR_CN_ID, 0xA0, Common_read_region},
+	{IMX355_AAC_ULTRA_SENSOR_CN_ID, 0xA0, Common_read_region},
 #endif
 	{IMX519_SENSOR_ID, 0xA0, Common_read_region},
 	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},

mt6768-S/android_kernel_xiaomi_mt6768

drivers/misc/mediatek/cam_cal/src/common/v1/eeprom_driver.c
@@ -621,6 +621,10 @@ static long EEPROM_drv_ioctl(struct file *file,
 		}
 
 		if (pcmdInf != NULL && g_lastDevID != ptempbuf->deviceID) {
+			#ifdef CONFIG_TARGET_PRODUCT_SELENE
+			//depth camera otp is in sensor,unable to get data due to register i2c fail,so disable it
+			if(ptempbuf->deviceID != 8) return -EFAULT;
+			#endif
 			if (EEPROM_set_i2c_bus(ptempbuf->deviceID,
 					       pcmdInf) != 0) {
 				pr_debug("deviceID Error!\n");

drivers/misc/mediatek/cam_cal/src/mt6768/cam_cal_list.c
@@ -27,6 +27,11 @@ extern unsigned int ov8856_qtech_front_read_otp_info(struct i2c_client *client, 
 #endif
 #endif
 
+#ifdef CONFIG_TARGET_PRODUCT_SELENE
+extern unsigned int gc02m1_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+extern unsigned int ov02b1b_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+#endif
+
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	/*Below is commom sensor */
 #ifdef CONFIG_TARGET_PRODUCT_LANCELOT
@@ -39,6 +44,34 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{OV8856_QTECH_FRONT_SENSOR_ID, 0x6C, ov8856_qtech_front_read_otp_info},
 	{GC5035_OFILM_SENSOR_ID, 0xA4, Common_read_region},
 	{GC5035_QTECH_SENSOR_ID, 0xA4, Common_read_region},
+#endif
+#ifdef CONFIG_TARGET_PRODUCT_SELENE
+	{OV50C40_OFILM_MAIN_SENSOR_ID, 0xA2, Common_read_region},
+	{S5KJN1_OFILM_MAIN_SENSOR_ID, 0xA2, Common_read_region},
+	{OV50C40_QTECH_MAIN_SENSOR_ID, 0xA2, Common_read_region},
+	{GC02M1_MACRO_AAC_SENSOR_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SY_SENSOR_ID, 0xA4, Common_read_region},
+	{IMX355_SUNNY_ULTRA_SENSOR_ID, 0xA0, Common_read_region},
+	{IMX355_AAC_ULTRA_SENSOR_ID, 0xA0, Common_read_region},
+	{OV8856_OFILM_FRONT_SENSOR_ID, 0xA2, Common_read_region},
+	{OV8856_AAC_FRONT_SENSOR_ID, 0xA2, Common_read_region},
+	{GC02M1B_SENSOR_ID1, 0xA2, gc02m1_read_otp_info},
+	{OV02B1B_OFILM_SENSOR_ID, 0xA2, ov02b1b_read_otp_info},
+	{OV50C40_OFILM_MAIN_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{S5KJN1_OFILM_MAIN_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV50C40_QTECH_MAIN_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{GC02M1_MACRO_AAC_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SY_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{IMX355_SUNNY_ULTRA_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{IMX355_AAC_ULTRA_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{OV8856_OFILM_FRONT_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV8856_AAC_FRONT_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{GC02M1B_SUNNY_SENSOR_INDIA_ID, 0xA2, gc02m1_read_otp_info},
+	{OV02B1B_OFILM_SENSOR_INDIA_ID, 0xA2, ov02b1b_read_otp_info},
+	{S5KJN1_OFILM_MAIN_SENSOR_CN_ID, 0xA2, Common_read_region},
+	{OV50C40_QTECH_MAIN_SENSOR_CN_ID, 0xA2, Common_read_region},
+	{IMX355_SUNNY_ULTRA_SENSOR_CN_ID, 0xA0, Common_read_region},
+	{IMX355_AAC_ULTRA_SENSOR_CN_ID, 0xA0, Common_read_region},
 #endif
 	{IMX519_SENSOR_ID, 0xA0, Common_read_region},
 	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},