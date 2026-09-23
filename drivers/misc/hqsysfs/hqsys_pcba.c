mt6768-S/android_kernel_xiaomi_mt6768-k419

hqsysfs: Update hqsysfs from selene-r-oss-update
Change-Id: I513f7fd2b1747e9524d694bbee2e46fec50a101e

drivers/misc/hqsysfs/hqsys_pcba.c
@@ -83,6 +83,12 @@ static board_id_map_t PCBA_DETECT_POCO_INDIA[] = {
 	{406, 500, PCBA_J19P_POCO_MP_INDIA},
 };
 
+#elif defined(TARGET_PRODUCT_SELENE)
+int hq_selene_pcba_config;
+
+static int selene_pcba_config;
+static int selene_pcba_stage;
+static int selene_pcba_count;
 
 #else
 static const board_id_map_t board_id_map[] = {
@@ -118,13 +124,15 @@ static const board_id_map_t j15n_board_id_map_ext[] = {
 
 static int __init get_pcba_config(char *p)
 {
-	char pcba[10];
+	int ret;
 
-	strlcpy(pcba, p, sizeof(pcba));
+	ret = kstrtoint(p, 10, &pcba_config);
+	if (ret) {
+		printk("[%s]: invalid pcba_config: %s\n", __func__, p);
+		return -EINVAL;
+	}
 
-	printk("[%s]: pcba = %s\n", __func__, pcba);
-
-	pcba_config = pcba[0] - '0';
+	printk("[%s]: pcba_config = %d\n", __func__, pcba_config);
 
 	return 0;
 }
@@ -218,14 +226,76 @@ static bool read_pcba_config_j19(void)
 	pr_err("[%s] read_pcba_config huaqin_pcba_config: 0x%x\n", __func__, huaqin_pcba_config);
 	return true;
 }
+
+#elif defined(TARGET_PRODUCT_SELENE)
+static int __init get_selene_pcba_config(char *p)
+{
+	int ret;
+
+	ret = kstrtoint(p, 10, &selene_pcba_config);
+	if (ret) {
+		printk("[%s]: invalid pcba_config: %s\n", __func__, p);
+		return -EINVAL;
+	}
+
+	printk("[%s]: pcba config = %d\n", __func__, selene_pcba_config);
+
+	hq_selene_pcba_config = selene_pcba_config;
+
+	return 0;
+}
+early_param("pcba_config", get_selene_pcba_config);
+
+static int __init get_selene_pcba_stage(char *p)
+{
+	char stage[10];
+
+	strlcpy(stage, p, sizeof(stage));
+
+	if (kstrtoint(stage, 10, &selene_pcba_stage))
+		return -1;
+
+	printk("[%s]: pcba stage = %d\n", __func__, selene_pcba_stage);
+
+	return 0;
+}
+early_param("pcba_stage", get_selene_pcba_stage);
+
+static int __init get_selene_pcba_count(char *p)
+{
+	char count[10];
+
+	strlcpy(count, p, sizeof(count));
+
+	if (kstrtoint(count, 10, &selene_pcba_count))
+		return -1;
+
+	printk("[%s]: pcba count = %d\n", __func__, selene_pcba_count);
+
+	return 0;
+}
+early_param("pcba_count", get_selene_pcba_count);
+
+static bool read_pcba_config_k19a(void)
+{
+	if (selene_pcba_config == PCBA_UNKNOW) {
+		huaqin_pcba_config = PCBA_UNKNOW;
+		return false;
+	}
+	huaqin_pcba_config = (selene_pcba_stage - 1) * selene_pcba_count + selene_pcba_config;
+	printk("[%s]: huaqin_pcba_config = %d\n", __func__, huaqin_pcba_config);
+	return true;
+}
+
 #else
+
 static bool read_pcba_config(void)
 {
 	int ret = 0;
 	int i = 0, map_size = 0;
 	int auxadc_voltage;
 	int hw_id_gpio, board_id3_gpio;
-	int hw_id_gpio_value, board_id3_gpio_value;
+	int hw_id_gpio_value = 0, board_id3_gpio_value = 0;
 	struct iio_channel *channel;
 	struct device_node *board_id_node;
 	struct platform_device *board_id_dev;
@@ -366,6 +436,8 @@ static int board_id_probe(struct platform_device *pdev)
 #if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
 
 	read_pcba_config_j19();
+#elif defined(TARGET_PRODUCT_SELENE)
+	read_pcba_config_k19a();
 #else
 	read_pcba_config();
 #endif

drivers/misc/hqsysfs/hqsys_pcba.h
@@ -42,6 +42,97 @@ typedef enum {
 	PCBA_J19P_INDIA,
 	PCBA_J19P_POCO_INDIA,
 } PCBA_J19_CONFIG;
+#elif defined(TARGET_PRODUCT_SELENE)
+typedef enum
+{
+	PCBA_UNKNOW = 0,
+
+	PCBA_K19A_P0_GLOBAL = 1,
+	PCBA_K19A_P0_LA,
+	PCBA_K19B_P0_IN,
+	PCBA_K19B_P0_CN,
+	PCBA_K19D_P0_GLOBAL,
+	PCBA_K19C_P0_GLOBAL,
+	PCBA_K19C_P0_IN,
+	PCBA_K19L_P0_LA,
+	PCBA_K19S_P0_CN,
+	PCBA_K19S_P0_GLOBAL,
+	PCBA_K19T_P0_IN,
+	PCBA_K19U_P0_GLOBAL,
+	PCBA_K19V_P0_LA,
+
+	PCBA_K19A_P0_1_GLOBAL = 14,
+	PCBA_K19A_P0_1_LA,
+	PCBA_K19B_P0_1_IN,
+	PCBA_K19B_P0_1_CN,
+	PCBA_K19D_P0_1_GLOBAL,
+	PCBA_K19C_P0_1_GLOBAL,
+	PCBA_K19C_P0_1_IN,
+	PCBA_K19L_P0_1_LA,
+	PCBA_K19S_P0_1_CN,
+	PCBA_K19S_P0_1_GLOBAL,
+	PCBA_K19T_P0_1_IN,
+	PCBA_K19U_P0_1_GLOBAL,
+	PCBA_K19V_P0_1_LA,
+
+	PCBA_K19A_P1_GLOBAL = 27,
+	PCBA_K19A_P1_LA,
+	PCBA_K19B_P1_IN,
+	PCBA_K19B_P1_CN,
+	PCBA_K19D_P1_GLOBAL,
+	PCBA_K19C_P1_GLOBAL,
+	PCBA_K19C_P1_IN,
+	PCBA_K19L_P1_LA,
+	PCBA_K19S_P1_CN,
+	PCBA_K19S_P1_GLOBAL,
+	PCBA_K19T_P1_IN,
+	PCBA_K19U_P1_GLOBAL,
+	PCBA_K19V_P1_LA,
+
+	PCBA_K19A_P1_1_GLOBAL = 40,
+	PCBA_K19A_P1_1_LA,
+	PCBA_K19B_P1_1_IN,
+	PCBA_K19B_P1_1_CN,
+	PCBA_K19D_P1_1_GLOBAL,
+	PCBA_K19C_P1_1_GLOBAL,
+	PCBA_K19C_P1_1_IN,
+	PCBA_K19L_P1_1_LA,
+	PCBA_K19S_P1_1_CN,
+	PCBA_K19S_P1_1_GLOBAL,
+	PCBA_K19T_P1_1_IN,
+	PCBA_K19U_P1_1_GLOBAL,
+	PCBA_K19V_P1_1_LA,
+
+	PCBA_K19A_P2_GLOBAL = 53,
+	PCBA_K19A_P2_LA,
+	PCBA_K19B_P2_IN,
+	PCBA_K19B_P2_CN,
+	PCBA_K19D_P2_GLOBAL,
+	PCBA_K19C_P2_GLOBAL,
+	PCBA_K19C_P2_IN,
+	PCBA_K19L_P2_LA,
+	PCBA_K19S_P2_CN,
+	PCBA_K19S_P2_GLOBAL,
+	PCBA_K19T_P2_IN,
+	PCBA_K19U_P2_GLOBAL,
+	PCBA_K19V_P2_LA,
+
+	PCBA_K19A_MP_GLOBAL = 66,
+	PCBA_K19A_MP_LA,
+	PCBA_K19B_MP_IN,
+	PCBA_K19B_MP_CN,
+	PCBA_K19D_MP_GLOBAL,
+	PCBA_K19C_MP_GLOBAL,
+	PCBA_K19C_MP_IN,
+	PCBA_K19L_MP_LA,
+	PCBA_K19S_MP_CN,
+	PCBA_K19S_MP_GLOBAL,
+	PCBA_K19T_MP_IN,
+	PCBA_K19U_MP_GLOBAL,
+	PCBA_K19V_MP_LA,
+
+	PCBA_END,
+} PCBA_CONFIG;
 #else
 typedef enum {
 	PCBA_UNKNOW = 0,

drivers/misc/hqsysfs/hqsysfs.c
@@ -45,6 +45,7 @@ static HW_INFO(HWID_FP, fingerprint);
 //static HW_INFO(HWID_TEE,tee);
 static HW_INFO(HWID_PCBA, pcba_config);
 
+static HW_INFO(HWID_PMIC_VERSION, pmic_version);
 static HW_INFO(HWID_AUDIO, audio_PA);
 
 #if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
@@ -71,6 +72,92 @@ struct pcba_info pcba[] = {
 	{PCBA_J19P_MP_INDIA, "PCBA_J19P_MP_INDIA"},
 	{PCBA_J19P_POCO_MP_INDIA, "PCBA_J19P_POCO_MP_INDIA"},
 
+};
+#elif defined(TARGET_PRODUCT_SELENE)
+struct pcba_info pcba[] = {
+	{PCBA_K19A_P0_GLOBAL, "PCBA_K19A_P0_GLOBAL"},
+	{PCBA_K19A_P0_LA, "PCBA_K19A_P0_LA"},
+	{PCBA_K19B_P0_IN, "PCBA_K19B_P0_IN"},
+	{PCBA_K19B_P0_CN, "PCBA_K19B_P0_CN"},
+	{PCBA_K19D_P0_GLOBAL, "PCBA_K19D_P0_GLOBAL"},
+	{PCBA_K19C_P0_GLOBAL, "PCBA_K19C_P0_GLOBAL"},
+	{PCBA_K19C_P0_IN, "PCBA_K19C_P0_IN"},
+	{PCBA_K19L_P0_LA, "PCBA_K19L_P0_LA"},
+	{PCBA_K19S_P0_CN, "PCBA_K19S_P0_CN"},
+	{PCBA_K19S_P0_GLOBAL, "PCBA_K19S_P0_GLOBAL"},
+	{PCBA_K19T_P0_IN, "PCBA_K19T_P0_IN"},
+	{PCBA_K19U_P0_GLOBAL, "PCBA_K19U_P0_GLOBAL"},
+	{PCBA_K19V_P0_LA, "PCBA_K19V_P0_LA"},
+
+	{PCBA_K19A_P0_1_GLOBAL, "PCBA_K19A_P0-1_GLOBAL"},
+	{PCBA_K19A_P0_1_LA, "PCBA_K19A_P0-1_LA"},
+	{PCBA_K19B_P0_1_IN, "PCBA_K19B_P0-1_IN"},
+	{PCBA_K19B_P0_1_CN, "PCBA_K19B_P0-1_CN"},
+	{PCBA_K19D_P0_1_GLOBAL, "PCBA_K19D_P0-1_GLOBAL"},
+	{PCBA_K19C_P0_1_GLOBAL, "PCBA_K19C_P0-1_GLOBAL"},
+	{PCBA_K19C_P0_1_IN, "PCBA_K19C_P0-1_IN"},
+	{PCBA_K19L_P0_1_LA, "PCBA_K19L_P0-1_LA"},
+	{PCBA_K19S_P0_1_CN, "PCBA_K19S_P0-1_CN"},
+	{PCBA_K19S_P0_1_GLOBAL, "PCBA_K19S_P0-1_GLOBAL"},
+	{PCBA_K19T_P0_1_IN, "PCBA_K19T_P0-1_IN"},
+	{PCBA_K19U_P0_1_GLOBAL, "PCBA_K19U_P0-1_GLOBAL"},
+	{PCBA_K19V_P0_1_LA, "PCBA_K19V_P0-1_LA"},
+
+	{PCBA_K19A_P1_GLOBAL, "PCBA_K19A_P1_GLOBAL"},
+	{PCBA_K19A_P1_LA, "PCBA_K19A_P1_LA"},
+	{PCBA_K19B_P1_IN, "PCBA_K19B_P1_IN"},
+	{PCBA_K19B_P1_CN, "PCBA_K19B_P1_CN"},
+	{PCBA_K19D_P1_GLOBAL, "PCBA_K19D_P1_GLOBAL"},
+	{PCBA_K19C_P1_GLOBAL, "PCBA_K19C_P1_GLOBAL"},
+	{PCBA_K19C_P1_IN, "PCBA_K19C_P1_IN"},
+	{PCBA_K19L_P1_LA, "PCBA_K19L_P1_LA"},
+	{PCBA_K19S_P1_CN, "PCBA_K19S_P1_CN"},
+	{PCBA_K19S_P1_GLOBAL, "PCBA_K19S_P1_GLOBAL"},
+	{PCBA_K19T_P1_IN, "PCBA_K19T_P1_IN"},
+	{PCBA_K19U_P1_GLOBAL, "PCBA_K19U_P1_GLOBAL"},
+	{PCBA_K19V_P1_LA, "PCBA_K19V_P1_LA"},
+
+	{PCBA_K19A_P1_1_GLOBAL, "PCBA_K19A_P1-1_GLOBAL"},
+	{PCBA_K19A_P1_1_LA, "PCBA_K19A_P1-1_LA"},
+	{PCBA_K19B_P1_1_IN, "PCBA_K19B_P1-1_IN"},
+	{PCBA_K19B_P1_1_CN, "PCBA_K19B_P1-1_CN"},
+	{PCBA_K19D_P1_1_GLOBAL, "PCBA_K19D_P1-1_GLOBAL"},
+	{PCBA_K19C_P1_1_GLOBAL, "PCBA_K19C_P1-1_GLOBAL"},
+	{PCBA_K19C_P1_1_IN, "PCBA_K19C_P1-1_IN"},
+	{PCBA_K19L_P1_1_LA, "PCBA_K19L_P1-1_LA"},
+	{PCBA_K19S_P1_1_CN, "PCBA_K19S_P1-1_CN"},
+	{PCBA_K19S_P1_1_GLOBAL, "PCBA_K19S_P1-1_GLOBAL"},
+	{PCBA_K19T_P1_1_IN, "PCBA_K19T_P1-1_IN"},
+	{PCBA_K19U_P1_1_GLOBAL, "PCBA_K19U_P1-1_GLOBAL"},
+	{PCBA_K19V_P1_1_LA, "PCBA_K19V_P1-1_LA"},
+
+	{PCBA_K19A_P2_GLOBAL, "PCBA_K19A_P2_GLOBAL"},
+	{PCBA_K19A_P2_LA, "PCBA_K19A_P2_LA"},
+	{PCBA_K19B_P2_IN, "PCBA_K19B_P2_IN"},
+	{PCBA_K19B_P2_CN, "PCBA_K19B_P2_CN"},
+	{PCBA_K19D_P2_GLOBAL, "PCBA_K19D_P2_GLOBAL"},
+	{PCBA_K19C_P2_GLOBAL, "PCBA_K19C_P2_GLOBAL"},
+	{PCBA_K19C_P2_IN, "PCBA_K19C_P2_IN"},
+	{PCBA_K19L_P2_LA, "PCBA_K19L_P2_LA"},
+	{PCBA_K19S_P2_CN, "PCBA_K19S_P2_CN"},
+	{PCBA_K19S_P2_GLOBAL, "PCBA_K19S_P2_GLOBAL"},
+	{PCBA_K19T_P2_IN, "PCBA_K19T_P2_IN"},
+	{PCBA_K19U_P2_GLOBAL, "PCBA_K19U_P2_GLOBAL"},
+	{PCBA_K19V_P2_LA, "PCBA_K19V_P2_LA"},
+
+	{PCBA_K19A_MP_GLOBAL, "PCBA_K19A_MP_GLOBAL"},
+	{PCBA_K19A_MP_LA, "PCBA_K19A_MP_LA"},
+	{PCBA_K19B_MP_IN, "PCBA_K19B_MP_IN"},
+	{PCBA_K19B_MP_CN, "PCBA_K19B_MP_CN"},
+	{PCBA_K19D_MP_GLOBAL, "PCBA_K19D_MP_GLOBAL"},
+	{PCBA_K19C_MP_GLOBAL, "PCBA_K19C_MP_GLOBAL"},
+	{PCBA_K19C_MP_IN, "PCBA_K19C_MP_IN"},
+	{PCBA_K19L_MP_LA, "PCBA_K19L_MP_LA"},
+	{PCBA_K19S_MP_CN, "PCBA_K19S_MP_CN"},
+	{PCBA_K19S_MP_GLOBAL, "PCBA_K19S_MP_GLOBAL"},
+	{PCBA_K19T_MP_IN, "PCBA_K19T_MP_IN"},
+	{PCBA_K19U_MP_GLOBAL, "PCBA_K19U_MP_GLOBAL"},
+	{PCBA_K19V_MP_LA, "PCBA_K19V_MP_LA"},
 };
 #else
 struct pcba_info pcba[] = {
@@ -124,6 +211,7 @@ static struct attribute *huaqin_attrs[] = {
 	&hw_info_nfc.attr,
 	&hw_info_fingerprint.attr,
 	&hw_info_pcba_config.attr,
+	&hw_info_pmic_version.attr,
 //	&hw_info_tee.attr,
 	&hw_info_audio_PA.attr,
 	NULL
@@ -173,6 +261,8 @@ static ssize_t huaqin_show(struct kobject *kobj, struct attribute *a, char *buf)
 		}
 
 		count = sprintf(buf, "%s\n", "PCBA_UNKONW");
+		} else if (HWID_PMIC_VERSION == hw->hw_id) {
+			count = sprintf(buf, "%s\n", "PMIC: MT6358 V1.0");
 		} else{
 
 		if (0 == hw->hw_exist) {

mt6768-S/android_kernel_xiaomi_mt6768

drivers/misc/hqsysfs/hqsys_pcba.c
@@ -34,7 +34,7 @@ typedef struct {
 	int voltage_max;
 	PCBA_CONFIG version;
 } board_id_map_t;
-#if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
+#if defined(CONFIG_TARGET_PRODUCT_LANCELOT)
 
 static int pcba_config;
 static board_id_map_t PCBA_DETECT_LANCELOT_CN[] = {
@@ -83,6 +83,12 @@ static board_id_map_t PCBA_DETECT_POCO_INDIA[] = {
 	{406, 500, PCBA_J19P_POCO_MP_INDIA},
 };
 
+#elif defined(CONFIG_TARGET_PRODUCT_SELENE)
+int hq_selene_pcba_config;
+
+static int selene_pcba_config;
+static int selene_pcba_stage;
+static int selene_pcba_count;
 
 #else
 static const board_id_map_t board_id_map[] = {
@@ -114,17 +120,19 @@ static const board_id_map_t j15n_board_id_map_ext[] = {
 };
 #endif
 
-#if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
+#if defined(CONFIG_TARGET_PRODUCT_LANCELOT)
 
 static int __init get_pcba_config(char *p)
 {
-	char pcba[10];
+	int ret;
 
-	strlcpy(pcba, p, sizeof(pcba));
+	ret = kstrtoint(p, 10, &pcba_config);
+	if (ret) {
+		printk("[%s]: invalid pcba_config: %s\n", __func__, p);
+		return -EINVAL;
+	}
 
-	printk("[%s]: pcba = %s\n", __func__, pcba);
-
-	pcba_config = pcba[0] - '0';
+	printk("[%s]: pcba_config = %d\n", __func__, pcba_config);
 
 	return 0;
 }
@@ -218,14 +226,76 @@ static bool read_pcba_config_j19(void)
 	pr_err("[%s] read_pcba_config huaqin_pcba_config: 0x%x\n", __func__, huaqin_pcba_config);
 	return true;
 }
+
+#elif defined(CONFIG_TARGET_PRODUCT_SELENE)
+static int __init get_selene_pcba_config(char *p)
+{
+	int ret;
+
+	ret = kstrtoint(p, 10, &selene_pcba_config);
+	if (ret) {
+		printk("[%s]: invalid pcba_config: %s\n", __func__, p);
+		return -EINVAL;
+	}
+
+	printk("[%s]: pcba config = %d\n", __func__, selene_pcba_config);
+
+	hq_selene_pcba_config = selene_pcba_config;
+
+	return 0;
+}
+early_param("pcba_config", get_selene_pcba_config);
+
+static int __init get_selene_pcba_stage(char *p)
+{
+	char stage[10];
+
+	strlcpy(stage, p, sizeof(stage));
+
+	if (kstrtoint(stage, 10, &selene_pcba_stage))
+		return -1;
+
+	printk("[%s]: pcba stage = %d\n", __func__, selene_pcba_stage);
+
+	return 0;
+}
+early_param("pcba_stage", get_selene_pcba_stage);
+
+static int __init get_selene_pcba_count(char *p)
+{
+	char count[10];
+
+	strlcpy(count, p, sizeof(count));
+
+	if (kstrtoint(count, 10, &selene_pcba_count))
+		return -1;
+
+	printk("[%s]: pcba count = %d\n", __func__, selene_pcba_count);
+
+	return 0;
+}
+early_param("pcba_count", get_selene_pcba_count);
+
+static bool read_pcba_config_k19a(void)
+{
+	if (selene_pcba_config == PCBA_UNKNOW) {
+		huaqin_pcba_config = PCBA_UNKNOW;
+		return false;
+	}
+	huaqin_pcba_config = (selene_pcba_stage - 1) * selene_pcba_count + selene_pcba_config;
+	printk("[%s]: huaqin_pcba_config = %d\n", __func__, huaqin_pcba_config);
+	return true;
+}
+
 #else
+
 static bool read_pcba_config(void)
 {
 	int ret = 0;
 	int i = 0, map_size = 0;
 	int auxadc_voltage;
 	int hw_id_gpio, board_id3_gpio;
-	int hw_id_gpio_value, board_id3_gpio_value;
+	int hw_id_gpio_value = 0, board_id3_gpio_value = 0;
 	struct iio_channel *channel;
 	struct device_node *board_id_node;
 	struct platform_device *board_id_dev;
@@ -363,9 +433,11 @@ static int board_id_probe(struct platform_device *pdev)
 		pr_err("[%s] Failed %d!!!\n", __func__, ret);
 		return ret;
 	}
-#if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
+#if defined(CONFIG_TARGET_PRODUCT_LANCELOT)
 
 	read_pcba_config_j19();
+#elif defined(CONFIG_TARGET_PRODUCT_SELENE)
+	read_pcba_config_k19a();
 #else
 	read_pcba_config();
 #endif

drivers/misc/hqsysfs/hqsys_pcba.h
@@ -1,7 +1,7 @@
 #ifndef HQSYS_PCBA
 #define HQSYS_PCBA
 
-#if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
+#if defined(CONFIG_TARGET_PRODUCT_LANCELOT)
 
 typedef enum {
 	PCBA_UNKNOW = 0,
@@ -42,6 +42,97 @@ typedef enum {
 	PCBA_J19P_INDIA,
 	PCBA_J19P_POCO_INDIA,
 } PCBA_J19_CONFIG;
+#elif defined(CONFIG_TARGET_PRODUCT_SELENE)
+typedef enum
+{
+	PCBA_UNKNOW = 0,
+
+	PCBA_K19A_P0_GLOBAL = 1,
+	PCBA_K19A_P0_LA,
+	PCBA_K19B_P0_IN,
+	PCBA_K19B_P0_CN,
+	PCBA_K19D_P0_GLOBAL,
+	PCBA_K19C_P0_GLOBAL,
+	PCBA_K19C_P0_IN,
+	PCBA_K19L_P0_LA,
+	PCBA_K19S_P0_CN,
+	PCBA_K19S_P0_GLOBAL,
+	PCBA_K19T_P0_IN,
+	PCBA_K19U_P0_GLOBAL,
+	PCBA_K19V_P0_LA,
+
+	PCBA_K19A_P0_1_GLOBAL = 14,
+	PCBA_K19A_P0_1_LA,
+	PCBA_K19B_P0_1_IN,
+	PCBA_K19B_P0_1_CN,
+	PCBA_K19D_P0_1_GLOBAL,
+	PCBA_K19C_P0_1_GLOBAL,
+	PCBA_K19C_P0_1_IN,
+	PCBA_K19L_P0_1_LA,
+	PCBA_K19S_P0_1_CN,
+	PCBA_K19S_P0_1_GLOBAL,
+	PCBA_K19T_P0_1_IN,
+	PCBA_K19U_P0_1_GLOBAL,
+	PCBA_K19V_P0_1_LA,
+
+	PCBA_K19A_P1_GLOBAL = 27,
+	PCBA_K19A_P1_LA,
+	PCBA_K19B_P1_IN,
+	PCBA_K19B_P1_CN,
+	PCBA_K19D_P1_GLOBAL,
+	PCBA_K19C_P1_GLOBAL,
+	PCBA_K19C_P1_IN,
+	PCBA_K19L_P1_LA,
+	PCBA_K19S_P1_CN,
+	PCBA_K19S_P1_GLOBAL,
+	PCBA_K19T_P1_IN,
+	PCBA_K19U_P1_GLOBAL,
+	PCBA_K19V_P1_LA,
+
+	PCBA_K19A_P1_1_GLOBAL = 40,
+	PCBA_K19A_P1_1_LA,
+	PCBA_K19B_P1_1_IN,
+	PCBA_K19B_P1_1_CN,
+	PCBA_K19D_P1_1_GLOBAL,
+	PCBA_K19C_P1_1_GLOBAL,
+	PCBA_K19C_P1_1_IN,
+	PCBA_K19L_P1_1_LA,
+	PCBA_K19S_P1_1_CN,
+	PCBA_K19S_P1_1_GLOBAL,
+	PCBA_K19T_P1_1_IN,
+	PCBA_K19U_P1_1_GLOBAL,
+	PCBA_K19V_P1_1_LA,
+
+	PCBA_K19A_P2_GLOBAL = 53,
+	PCBA_K19A_P2_LA,
+	PCBA_K19B_P2_IN,
+	PCBA_K19B_P2_CN,
+	PCBA_K19D_P2_GLOBAL,
+	PCBA_K19C_P2_GLOBAL,
+	PCBA_K19C_P2_IN,
+	PCBA_K19L_P2_LA,
+	PCBA_K19S_P2_CN,
+	PCBA_K19S_P2_GLOBAL,
+	PCBA_K19T_P2_IN,
+	PCBA_K19U_P2_GLOBAL,
+	PCBA_K19V_P2_LA,
+
+	PCBA_K19A_MP_GLOBAL = 66,
+	PCBA_K19A_MP_LA,
+	PCBA_K19B_MP_IN,
+	PCBA_K19B_MP_CN,
+	PCBA_K19D_MP_GLOBAL,
+	PCBA_K19C_MP_GLOBAL,
+	PCBA_K19C_MP_IN,
+	PCBA_K19L_MP_LA,
+	PCBA_K19S_MP_CN,
+	PCBA_K19S_MP_GLOBAL,
+	PCBA_K19T_MP_IN,
+	PCBA_K19U_MP_GLOBAL,
+	PCBA_K19V_MP_LA,
+
+	PCBA_END,
+} PCBA_CONFIG;
 #else
 typedef enum {
 	PCBA_UNKNOW = 0,

drivers/misc/hqsysfs/hqsysfs.c
@@ -45,9 +45,10 @@ static HW_INFO(HWID_FP, fingerprint);
 //static HW_INFO(HWID_TEE,tee);
 static HW_INFO(HWID_PCBA, pcba_config);
 
+static HW_INFO(HWID_PMIC_VERSION, pmic_version);
 static HW_INFO(HWID_AUDIO, audio_PA);
 
-#if defined(TARGET_PRODUCT_LANCELOT) || defined(TARGET_PRODUCT_SHIVA)
+#if defined(CONFIG_TARGET_PRODUCT_LANCELOT)
 
 struct pcba_info pcba[] = {
 	{PCBA_J19_P0_1_CN, "PCBA_J19_P0-1_CN"},
@@ -71,6 +72,92 @@ struct pcba_info pcba[] = {
 	{PCBA_J19P_MP_INDIA, "PCBA_J19P_MP_INDIA"},
 	{PCBA_J19P_POCO_MP_INDIA, "PCBA_J19P_POCO_MP_INDIA"},
 
+};
+#elif defined(CONFIG_TARGET_PRODUCT_SELENE)
+struct pcba_info pcba[] = {
+	{PCBA_K19A_P0_GLOBAL, "PCBA_K19A_P0_GLOBAL"},
+	{PCBA_K19A_P0_LA, "PCBA_K19A_P0_LA"},
+	{PCBA_K19B_P0_IN, "PCBA_K19B_P0_IN"},
+	{PCBA_K19B_P0_CN, "PCBA_K19B_P0_CN"},
+	{PCBA_K19D_P0_GLOBAL, "PCBA_K19D_P0_GLOBAL"},
+	{PCBA_K19C_P0_GLOBAL, "PCBA_K19C_P0_GLOBAL"},
+	{PCBA_K19C_P0_IN, "PCBA_K19C_P0_IN"},
+	{PCBA_K19L_P0_LA, "PCBA_K19L_P0_LA"},
+	{PCBA_K19S_P0_CN, "PCBA_K19S_P0_CN"},
+	{PCBA_K19S_P0_GLOBAL, "PCBA_K19S_P0_GLOBAL"},
+	{PCBA_K19T_P0_IN, "PCBA_K19T_P0_IN"},
+	{PCBA_K19U_P0_GLOBAL, "PCBA_K19U_P0_GLOBAL"},
+	{PCBA_K19V_P0_LA, "PCBA_K19V_P0_LA"},
+
+	{PCBA_K19A_P0_1_GLOBAL, "PCBA_K19A_P0-1_GLOBAL"},
+	{PCBA_K19A_P0_1_LA, "PCBA_K19A_P0-1_LA"},
+	{PCBA_K19B_P0_1_IN, "PCBA_K19B_P0-1_IN"},
+	{PCBA_K19B_P0_1_CN, "PCBA_K19B_P0-1_CN"},
+	{PCBA_K19D_P0_1_GLOBAL, "PCBA_K19D_P0-1_GLOBAL"},
+	{PCBA_K19C_P0_1_GLOBAL, "PCBA_K19C_P0-1_GLOBAL"},
+	{PCBA_K19C_P0_1_IN, "PCBA_K19C_P0-1_IN"},
+	{PCBA_K19L_P0_1_LA, "PCBA_K19L_P0-1_LA"},
+	{PCBA_K19S_P0_1_CN, "PCBA_K19S_P0-1_CN"},
+	{PCBA_K19S_P0_1_GLOBAL, "PCBA_K19S_P0-1_GLOBAL"},
+	{PCBA_K19T_P0_1_IN, "PCBA_K19T_P0-1_IN"},
+	{PCBA_K19U_P0_1_GLOBAL, "PCBA_K19U_P0-1_GLOBAL"},
+	{PCBA_K19V_P0_1_LA, "PCBA_K19V_P0-1_LA"},
+
+	{PCBA_K19A_P1_GLOBAL, "PCBA_K19A_P1_GLOBAL"},
+	{PCBA_K19A_P1_LA, "PCBA_K19A_P1_LA"},
+	{PCBA_K19B_P1_IN, "PCBA_K19B_P1_IN"},
+	{PCBA_K19B_P1_CN, "PCBA_K19B_P1_CN"},
+	{PCBA_K19D_P1_GLOBAL, "PCBA_K19D_P1_GLOBAL"},
+	{PCBA_K19C_P1_GLOBAL, "PCBA_K19C_P1_GLOBAL"},
+	{PCBA_K19C_P1_IN, "PCBA_K19C_P1_IN"},
+	{PCBA_K19L_P1_LA, "PCBA_K19L_P1_LA"},
+	{PCBA_K19S_P1_CN, "PCBA_K19S_P1_CN"},
+	{PCBA_K19S_P1_GLOBAL, "PCBA_K19S_P1_GLOBAL"},
+	{PCBA_K19T_P1_IN, "PCBA_K19T_P1_IN"},
+	{PCBA_K19U_P1_GLOBAL, "PCBA_K19U_P1_GLOBAL"},
+	{PCBA_K19V_P1_LA, "PCBA_K19V_P1_LA"},
+
+	{PCBA_K19A_P1_1_GLOBAL, "PCBA_K19A_P1-1_GLOBAL"},
+	{PCBA_K19A_P1_1_LA, "PCBA_K19A_P1-1_LA"},
+	{PCBA_K19B_P1_1_IN, "PCBA_K19B_P1-1_IN"},
+	{PCBA_K19B_P1_1_CN, "PCBA_K19B_P1-1_CN"},
+	{PCBA_K19D_P1_1_GLOBAL, "PCBA_K19D_P1-1_GLOBAL"},
+	{PCBA_K19C_P1_1_GLOBAL, "PCBA_K19C_P1-1_GLOBAL"},
+	{PCBA_K19C_P1_1_IN, "PCBA_K19C_P1-1_IN"},
+	{PCBA_K19L_P1_1_LA, "PCBA_K19L_P1-1_LA"},
+	{PCBA_K19S_P1_1_CN, "PCBA_K19S_P1-1_CN"},
+	{PCBA_K19S_P1_1_GLOBAL, "PCBA_K19S_P1-1_GLOBAL"},
+	{PCBA_K19T_P1_1_IN, "PCBA_K19T_P1-1_IN"},
+	{PCBA_K19U_P1_1_GLOBAL, "PCBA_K19U_P1-1_GLOBAL"},
+	{PCBA_K19V_P1_1_LA, "PCBA_K19V_P1-1_LA"},
+
+	{PCBA_K19A_P2_GLOBAL, "PCBA_K19A_P2_GLOBAL"},
+	{PCBA_K19A_P2_LA, "PCBA_K19A_P2_LA"},
+	{PCBA_K19B_P2_IN, "PCBA_K19B_P2_IN"},
+	{PCBA_K19B_P2_CN, "PCBA_K19B_P2_CN"},
+	{PCBA_K19D_P2_GLOBAL, "PCBA_K19D_P2_GLOBAL"},
+	{PCBA_K19C_P2_GLOBAL, "PCBA_K19C_P2_GLOBAL"},
+	{PCBA_K19C_P2_IN, "PCBA_K19C_P2_IN"},
+	{PCBA_K19L_P2_LA, "PCBA_K19L_P2_LA"},
+	{PCBA_K19S_P2_CN, "PCBA_K19S_P2_CN"},
+	{PCBA_K19S_P2_GLOBAL, "PCBA_K19S_P2_GLOBAL"},
+	{PCBA_K19T_P2_IN, "PCBA_K19T_P2_IN"},
+	{PCBA_K19U_P2_GLOBAL, "PCBA_K19U_P2_GLOBAL"},
+	{PCBA_K19V_P2_LA, "PCBA_K19V_P2_LA"},
+
+	{PCBA_K19A_MP_GLOBAL, "PCBA_K19A_MP_GLOBAL"},
+	{PCBA_K19A_MP_LA, "PCBA_K19A_MP_LA"},
+	{PCBA_K19B_MP_IN, "PCBA_K19B_MP_IN"},
+	{PCBA_K19B_MP_CN, "PCBA_K19B_MP_CN"},
+	{PCBA_K19D_MP_GLOBAL, "PCBA_K19D_MP_GLOBAL"},
+	{PCBA_K19C_MP_GLOBAL, "PCBA_K19C_MP_GLOBAL"},
+	{PCBA_K19C_MP_IN, "PCBA_K19C_MP_IN"},
+	{PCBA_K19L_MP_LA, "PCBA_K19L_MP_LA"},
+	{PCBA_K19S_MP_CN, "PCBA_K19S_MP_CN"},
+	{PCBA_K19S_MP_GLOBAL, "PCBA_K19S_MP_GLOBAL"},
+	{PCBA_K19T_MP_IN, "PCBA_K19T_MP_IN"},
+	{PCBA_K19U_MP_GLOBAL, "PCBA_K19U_MP_GLOBAL"},
+	{PCBA_K19V_MP_LA, "PCBA_K19V_MP_LA"},
 };
 #else
 struct pcba_info pcba[] = {
@@ -124,6 +211,7 @@ static struct attribute *huaqin_attrs[] = {
 	&hw_info_nfc.attr,
 	&hw_info_fingerprint.attr,
 	&hw_info_pcba_config.attr,
+	&hw_info_pmic_version.attr,
 //	&hw_info_tee.attr,
 	&hw_info_audio_PA.attr,
 	NULL
@@ -173,6 +261,8 @@ static ssize_t huaqin_show(struct kobject *kobj, struct attribute *a, char *buf)
 		}
 
 		count = sprintf(buf, "%s\n", "PCBA_UNKONW");
+		} else if (HWID_PMIC_VERSION == hw->hw_id) {
+			count = sprintf(buf, "%s\n", "PMIC: MT6358 V1.0");
 		} else{
 
 		if (0 == hw->hw_exist) {