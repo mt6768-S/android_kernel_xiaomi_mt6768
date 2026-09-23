mt6768-S/android_kernel_xiaomi_mt6768-k419

imgsensor: Import Xiaomi changes
* From selene-r-oss-update

Change-Id: Ia0bb11c4b18c0b3137551694f6f9413f5c60e57b

drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor_hw.c
@@ -93,6 +93,11 @@ static enum IMGSENSOR_RETURN imgsensor_hw_power_sequence(
 	struct IMGSENSOR_HW_SENSOR_POWER *psensor_pwr =
 	    &phw->sensor_pwr[sensor_idx];
 
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	struct IMGSENSOR_HW_SENSOR_POWER *psensor_pwr_Mi =
+		&phw->sensor_pwr[IMGSENSOR_SENSOR_IDX_SUB2];
+#endif
+
 	struct IMGSENSOR_HW_POWER_SEQ    *ppwr_seq = ppower_sequence;
 	struct IMGSENSOR_HW_POWER_INFO   *ppwr_info;
 	struct IMGSENSOR_HW_DEVICE       *pdev;
@@ -137,6 +142,46 @@ static enum IMGSENSOR_RETURN imgsensor_hw_power_sequence(
 				    ppwr_info->pin_state_on);
 
 			mdelay(ppwr_info->pin_on_delay);
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+		if (sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN &&
+			ppwr_info->pin ==IMGSENSOR_HW_PIN_DOVDD)
+		{
+			while (ppwr_seq < ppower_sequence + IMGSENSOR_HW_SENSOR_MAX_NUM &&
+				ppwr_seq->name != NULL) {
+				if (!strcmp(ppwr_seq->name, PLATFORM_POWER_SEQ_NAME)) {
+					if (sensor_idx == ppwr_seq->_idx)
+						break;
+				} else {
+					if (!strcmp(ppwr_seq->name, "gc02m1b_sunny_mipi_raw") || !strcmp(ppwr_seq->name, "ov02b1b_ofilm_mipi_raw"))
+						break;
+				}
+				ppwr_seq++;
+			}
+
+			pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+			if (pdev->set != NULL)
+				pdev->set(
+				    pdev->pinstance,
+				    IMGSENSOR_SENSOR_IDX_SUB2,
+				    IMGSENSOR_HW_PIN_DOVDD,
+				    IMGSENSOR_HW_PIN_STATE_LEVEL_1800);
+			}
+
+		if ((sensor_idx == IMGSENSOR_SENSOR_IDX_SUB || sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN2) &&
+		    ppwr_info->pin ==IMGSENSOR_HW_PIN_AVDD)
+		{
+		    pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+		    if (pdev->set != NULL)
+		        pdev->set(
+		            pdev->pinstance,
+		            IMGSENSOR_SENSOR_IDX_SUB2,
+		            IMGSENSOR_HW_PIN_DOVDD,
+		            IMGSENSOR_HW_PIN_STATE_LEVEL_1800);
+		}
+#endif
 		}
 
 		ppwr_info++;
@@ -159,6 +204,34 @@ static enum IMGSENSOR_RETURN imgsensor_hw_power_sequence(
 					    sensor_idx,
 					    ppwr_info->pin,
 					    ppwr_info->pin_state_off);
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	if (sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN &&
+		ppwr_info->pin ==IMGSENSOR_HW_PIN_DOVDD)
+	{
+		pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+				if (pdev->set != NULL)
+					pdev->set(
+					    pdev->pinstance,
+					    IMGSENSOR_SENSOR_IDX_SUB2,
+					    IMGSENSOR_HW_PIN_DOVDD,
+					    IMGSENSOR_HW_PIN_STATE_LEVEL_0);
+	}
+
+	if ((sensor_idx == IMGSENSOR_SENSOR_IDX_SUB || sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN2) &&
+	    ppwr_info->pin ==IMGSENSOR_HW_PIN_AVDD)
+	{
+	    pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+	    if (pdev->set != NULL)
+	        pdev->set(
+	            pdev->pinstance,
+	            IMGSENSOR_SENSOR_IDX_SUB2,
+	            IMGSENSOR_HW_PIN_DOVDD,
+	            IMGSENSOR_HW_PIN_STATE_LEVEL_0);
+	}
+#endif
 			}
 		}
 	}

drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor_sensor_list.c
@@ -76,7 +76,7 @@ struct IMGSENSOR_INIT_FUNC_LIST kdSensorList[MAX_NUM_OF_SUPPORT_SENSOR] = {
 	SENSOR_DRVNAME_GC5035_QTECH_MIPI_RAW,
 	GC5035_QTECH_MIPI_RAW_SensorInit},
 #endif
-#elif CONFIG_TARGET_PRODUCT_MERLINCOMMON
+#elif defined(CONFIG_TARGET_PRODUCT_MERLINCOMMON)
 // main
 #if defined(S5KGM1SP_MIPI_RAW)
 	{S5KGM1SP_SENSOR_ID,
@@ -136,6 +136,66 @@ struct IMGSENSOR_INIT_FUNC_LIST kdSensorList[MAX_NUM_OF_SUPPORT_SENSOR] = {
 	{OV2680_SUNNY_SENSOR_ID,
 	SENSOR_DRVNAME_OV2680_SUNNY_MIPI_RAW,
 	OV2680_SUNNY_MIPI_RAW_SensorInit},
+#endif
+#elif defined(CONFIG_TARGET_PRODUCT_SELENECOMMON)
+/*K19A  main*/
+#if defined(OV50C40_OFILM_MAIN_MIPI_RAW)
+	{OV50C40_OFILM_MAIN_SENSOR_ID,
+	SENSOR_DRVNAME_OV50C40_OFILM_MAIN_MIPI_RAW,
+	OV50C40_OFILM_MAIN_MIPI_RAW_SensorInit},
+#endif
+#if defined(S5KJN1_OFILM_MAIN_MIPI_RAW)
+	{S5KJN1_OFILM_MAIN_SENSOR_ID,
+	SENSOR_DRVNAME_S5KJN1_OFILM_MAIN_MIPI_RAW,
+	S5KJN1_OFILM_MAIN_MIPI_RAW_SensorInit},
+#endif
+#if defined(OV50C40_QTECH_MAIN_MIPI_RAW)
+	{OV50C40_QTECH_MAIN_SENSOR_ID,
+	SENSOR_DRVNAME_OV50C40_QTECH_MAIN_MIPI_RAW,
+	OV50C40_QTECH_MAIN_MIPI_RAW_SensorInit},
+#endif
+/*K19A front*/
+#if defined(OV8856_OFILM_FRONT_MIPI_RAW)
+	{OV8856_OFILM_FRONT_SENSOR_ID,
+	SENSOR_DRVNAME_OV8856_OFILM_FRONT_MIPI_RAW,
+	OV8856_OFILM_MIPI_RAW_SensorInit},
+#endif
+#if defined(OV8856_AAC_FRONT_MIPI_RAW)
+	{OV8856_AAC_FRONT_SENSOR_ID,
+	SENSOR_DRVNAME_OV8856_AAC_FRONT_MIPI_RAW,
+	OV8856_AAC_MIPI_RAW_SensorInit},
+#endif
+/*K19A macro*/
+#if defined(GC02M1_MACRO_AAC_MIPI_RAW)
+	{GC02M1_MACRO_AAC_SENSOR_ID,
+	SENSOR_DRVNAME_GC02M1_MACRO_AAC_MIPI_RAW,
+	GC02M1_MACRO_AAC_MIPI_RAW_SensorInit},
+#endif
+#if defined(GC02M1_MACRO_SY_MIPI_RAW)
+	{GC02M1_MACRO_SY_SENSOR_ID,
+	SENSOR_DRVNAME_GC02M1_MACRO_SY_MIPI_RAW,
+	GC02M1_MACRO_SY_MIPI_RAW_SensorInit},
+#endif
+/*K19A depth*/
+#if defined(GC02M1B_SUNNY_MIPI_RAW)
+	{GC02M1B_SENSOR_ID1,
+	SENSOR_DRVNAME_GC02M1B_SUNNY_MIPI_RAW,
+	GC02M1B_SUNNY_MIPI_RAW_SensorInit},
+#endif
+#if defined(OV02B1B_OFILM_MIPI_RAW)
+	{OV02B1B_OFILM_SENSOR_ID,
+	SENSOR_DRVNAME_OV02B1B_OFILM_MIPI_RAW,
+	OV02B1B_OFILM_MIPI_RAW_SensorInit},
+#endif
+#if defined(IMX355_SUNNY_ULTRA_MIPI_RAW)
+	{IMX355_SUNNY_ULTRA_SENSOR_ID,
+	SENSOR_DRVNAME_IMX355_SUNNY_ULTRA_MIPI_RAW,
+	IMX355_SUNNY_ULTRA_MIPI_RAW_SensorInit},
+#endif
+#if defined(IMX355_AAC_ULTRA_MIPI_RAW)
+	{IMX355_AAC_ULTRA_SENSOR_ID,
+	SENSOR_DRVNAME_IMX355_AAC_ULTRA_MIPI_RAW,
+	IMX355_AAC_ULTRA_MIPI_RAW_SensorInit},
 #endif
 #else
 #if defined(S5K4H7_MIPI_RAW)

mt6768-S/android_kernel_xiaomi_mt6768

drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor_hw.c
@@ -93,6 +93,11 @@ static enum IMGSENSOR_RETURN imgsensor_hw_power_sequence(
 	struct IMGSENSOR_HW_SENSOR_POWER *psensor_pwr =
 	    &phw->sensor_pwr[sensor_idx];
 
+#ifdef CONFIG_TARGET_PRODUCT_SELENE
+	struct IMGSENSOR_HW_SENSOR_POWER *psensor_pwr_Mi =
+		&phw->sensor_pwr[IMGSENSOR_SENSOR_IDX_SUB2];
+#endif
+
 	struct IMGSENSOR_HW_POWER_SEQ    *ppwr_seq = ppower_sequence;
 	struct IMGSENSOR_HW_POWER_INFO   *ppwr_info;
 	struct IMGSENSOR_HW_DEVICE       *pdev;
@@ -137,6 +142,46 @@ static enum IMGSENSOR_RETURN imgsensor_hw_power_sequence(
 				    ppwr_info->pin_state_on);
 
 			mdelay(ppwr_info->pin_on_delay);
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENE
+		if (sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN &&
+			ppwr_info->pin ==IMGSENSOR_HW_PIN_DOVDD)
+		{
+			while (ppwr_seq < ppower_sequence + IMGSENSOR_HW_SENSOR_MAX_NUM &&
+				ppwr_seq->name != NULL) {
+				if (!strcmp(ppwr_seq->name, PLATFORM_POWER_SEQ_NAME)) {
+					if (sensor_idx == ppwr_seq->_idx)
+						break;
+				} else {
+					if (!strcmp(ppwr_seq->name, "gc02m1b_sunny_mipi_raw") || !strcmp(ppwr_seq->name, "ov02b1b_ofilm_mipi_raw"))
+						break;
+				}
+				ppwr_seq++;
+			}
+
+			pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+			if (pdev->set != NULL)
+				pdev->set(
+				    pdev->pinstance,
+				    IMGSENSOR_SENSOR_IDX_SUB2,
+				    IMGSENSOR_HW_PIN_DOVDD,
+				    IMGSENSOR_HW_PIN_STATE_LEVEL_1800);
+			}
+
+		if ((sensor_idx == IMGSENSOR_SENSOR_IDX_SUB || sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN2) &&
+		    ppwr_info->pin ==IMGSENSOR_HW_PIN_AVDD)
+		{
+		    pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+		    if (pdev->set != NULL)
+		        pdev->set(
+		            pdev->pinstance,
+		            IMGSENSOR_SENSOR_IDX_SUB2,
+		            IMGSENSOR_HW_PIN_DOVDD,
+		            IMGSENSOR_HW_PIN_STATE_LEVEL_1800);
+		}
+#endif
 		}
 
 		ppwr_info++;
@@ -159,6 +204,34 @@ static enum IMGSENSOR_RETURN imgsensor_hw_power_sequence(
 					    sensor_idx,
 					    ppwr_info->pin,
 					    ppwr_info->pin_state_off);
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENE
+	if (sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN &&
+		ppwr_info->pin ==IMGSENSOR_HW_PIN_DOVDD)
+	{
+		pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+				if (pdev->set != NULL)
+					pdev->set(
+					    pdev->pinstance,
+					    IMGSENSOR_SENSOR_IDX_SUB2,
+					    IMGSENSOR_HW_PIN_DOVDD,
+					    IMGSENSOR_HW_PIN_STATE_LEVEL_0);
+	}
+
+	if ((sensor_idx == IMGSENSOR_SENSOR_IDX_SUB || sensor_idx == IMGSENSOR_SENSOR_IDX_MAIN2) &&
+	    ppwr_info->pin ==IMGSENSOR_HW_PIN_AVDD)
+	{
+	    pdev = phw->pdev[psensor_pwr_Mi->id[IMGSENSOR_HW_PIN_DOVDD]];
+
+	    if (pdev->set != NULL)
+	        pdev->set(
+	            pdev->pinstance,
+	            IMGSENSOR_SENSOR_IDX_SUB2,
+	            IMGSENSOR_HW_PIN_DOVDD,
+	            IMGSENSOR_HW_PIN_STATE_LEVEL_0);
+	}
+#endif
 			}
 		}
 	}

drivers/misc/mediatek/imgsensor/src/common/v1/imgsensor_sensor_list.c
@@ -137,6 +137,66 @@ struct IMGSENSOR_INIT_FUNC_LIST kdSensorList[MAX_NUM_OF_SUPPORT_SENSOR] = {
 	SENSOR_DRVNAME_OV2680_SUNNY_MIPI_RAW,
 	OV2680_SUNNY_MIPI_RAW_SensorInit},
 #endif
+#elif defined(CONFIG_TARGET_PRODUCT_SELENE)
+/*K19A  main*/
+#if defined(OV50C40_OFILM_MAIN_MIPI_RAW)
+	{OV50C40_OFILM_MAIN_SENSOR_ID,
+	SENSOR_DRVNAME_OV50C40_OFILM_MAIN_MIPI_RAW,
+	OV50C40_OFILM_MAIN_MIPI_RAW_SensorInit},
+#endif
+#if defined(S5KJN1_OFILM_MAIN_MIPI_RAW)
+	{S5KJN1_OFILM_MAIN_SENSOR_ID,
+	SENSOR_DRVNAME_S5KJN1_OFILM_MAIN_MIPI_RAW,
+	S5KJN1_OFILM_MAIN_MIPI_RAW_SensorInit},
+#endif
+#if defined(OV50C40_QTECH_MAIN_MIPI_RAW)
+	{OV50C40_QTECH_MAIN_SENSOR_ID,
+	SENSOR_DRVNAME_OV50C40_QTECH_MAIN_MIPI_RAW,
+	OV50C40_QTECH_MAIN_MIPI_RAW_SensorInit},
+#endif
+/*K19A front*/
+#if defined(OV8856_OFILM_FRONT_MIPI_RAW)
+	{OV8856_OFILM_FRONT_SENSOR_ID,
+	SENSOR_DRVNAME_OV8856_OFILM_FRONT_MIPI_RAW,
+	OV8856_OFILM_MIPI_RAW_SensorInit},
+#endif
+#if defined(OV8856_AAC_FRONT_MIPI_RAW)
+	{OV8856_AAC_FRONT_SENSOR_ID,
+	SENSOR_DRVNAME_OV8856_AAC_FRONT_MIPI_RAW,
+	OV8856_AAC_MIPI_RAW_SensorInit},
+#endif
+/*K19A macro*/
+#if defined(GC02M1_MACRO_AAC_MIPI_RAW)
+	{GC02M1_MACRO_AAC_SENSOR_ID,
+	SENSOR_DRVNAME_GC02M1_MACRO_AAC_MIPI_RAW,
+	GC02M1_MACRO_AAC_MIPI_RAW_SensorInit},
+#endif
+#if defined(GC02M1_MACRO_SY_MIPI_RAW)
+	{GC02M1_MACRO_SY_SENSOR_ID,
+	SENSOR_DRVNAME_GC02M1_MACRO_SY_MIPI_RAW,
+	GC02M1_MACRO_SY_MIPI_RAW_SensorInit},
+#endif
+/*K19A depth*/
+#if defined(GC02M1B_SUNNY_MIPI_RAW)
+	{GC02M1B_SENSOR_ID1,
+	SENSOR_DRVNAME_GC02M1B_SUNNY_MIPI_RAW,
+	GC02M1B_SUNNY_MIPI_RAW_SensorInit},
+#endif
+#if defined(OV02B1B_OFILM_MIPI_RAW)
+	{OV02B1B_OFILM_SENSOR_ID,
+	SENSOR_DRVNAME_OV02B1B_OFILM_MIPI_RAW,
+	OV02B1B_OFILM_MIPI_RAW_SensorInit},
+#endif
+#if defined(IMX355_SUNNY_ULTRA_MIPI_RAW)
+	{IMX355_SUNNY_ULTRA_SENSOR_ID,
+	SENSOR_DRVNAME_IMX355_SUNNY_ULTRA_MIPI_RAW,
+	IMX355_SUNNY_ULTRA_MIPI_RAW_SensorInit},
+#endif
+#if defined(IMX355_AAC_ULTRA_MIPI_RAW)
+	{IMX355_AAC_ULTRA_SENSOR_ID,
+	SENSOR_DRVNAME_IMX355_AAC_ULTRA_MIPI_RAW,
+	IMX355_AAC_ULTRA_MIPI_RAW_SensorInit},
+#endif
 #else
 #if defined(S5K4H7_MIPI_RAW)
 	{S5K4H7_SENSOR_ID,